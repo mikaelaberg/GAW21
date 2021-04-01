d6b2514c426bbb12b2e89536431dfce46693ffeb
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-04-01 13:06:04 -0400

    formating and graph selector works

--- citizen_scientist_dash/main.js
+++ citizen_scientist_dash/main.js
@@ -39,3 +39,45 @@ function changeGraph(){
 
 } */
 
+/*
+function Graph1(){
+    var graph1Source = "https://github.com/mikaelaberg/GAW21/blob/main/citizen_scientist_dash/images/Main%20Page.png";
+    var img = document.getElementById("Graph1")
+    img.src = graph1Source.replace('90x90', '225x225');
+    img.style.display= "block";
+
+}
+
+function Graph1(){
+    var graph1 = "https://github.com/mikaelaberg/GAW21/blob/main/citizen_scientist_dash/images/Main%20Page.png"
+    document.getElementById("Graph1").src = pic.replace('90x90', '225x225');
+    document.getElementById('Graph1').style.display='block';
+
+}
+
+function Graph2(){
+    var graph2Source = "https://github.com/mikaelaberg/GAW21/blob/main/citizen_scientist_dash/images/Web%201920%20%E2%80%93%201.png";
+    var img = document.getElementById("Graph2")
+    img.src = graph2Source.replace('90x90', '225x225');
+    img.style.display= "block";
+
+}
+
+function Graph3(){
+    var graph3Source = "https://github.com/mikaelaberg/GAW21/blob/main/citizen_scientist_dash/images/gvsu-logo.png";
+    var img = document.getElementById("Graph3")
+    img.src = graph3Source.replace('90x90', '225x225');
+    img.style.display= "block";
+
+}
+*/
+
+
+
+function changeGraph(graphSource){
+    var imgagecontainer = document.getElementById("GraphBase");
+    imgagecontainer.src = graphSource;
+    imgagecontainer.height = "500";
+    imgagecontainer.width = "500";
+    document.getElementById("GraphBase").innerHtml = imgagecontainer;
+}
