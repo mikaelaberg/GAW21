436d742bb1c3afde5c1e3da43b55369182439d6d
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-04-20 08:46:47 -0400

    compass is on there...not really working how we want it

--- citizen_scientist_dash/main.js
+++ citizen_scientist_dash/main.js
@@ -86,3 +86,127 @@ function setGauge2Value(gauge, value) {
   }
 
 setGauge2Value(gaugeElement2,json.Phycocyanin2m);
+
+
+
+
+/*https://github.com/jamesgpearce/compios5/blob/master/index.html
+window.addEventListener('load', function() {
+
+  var compass = document.body.appendChild(document.createElement('article'));
+  compass.id='compass';
+  var spinner = compass.appendChild(document.createElement('article'));
+  spinner.id='spinner';
+  var pin = spinner.appendChild(document.createElement('article'));
+  pin.id='pin';
+
+  for (var degrees=0, degree; degrees<360; degrees+=30) {
+      degree = spinner.appendChild(document.createElement('label'));
+      degree.className='degree';
+      degree.innerText = degrees;
+      degree.style.webkitTransform = 'rotateZ(' + degrees + 'deg)'
+  }
+  ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'].forEach(function(label, index) {
+      var main = ((index % 2)?'':' main');
+      point = spinner.appendChild(document.createElement('label'));
+      point.className='point' + main;
+      point.innerText = label;
+      point.style.webkitTransform = 'rotateZ(' + (index * 45) + 'deg)'
+      arrow = spinner.appendChild(document.createElement('div'));
+      arrow.className='arrow' + main;
+      arrow.style.webkitTransform = 'rotateZ(' + (index * 45) + 'deg)'
+  });
+
+  var lastHeading = 0;
+  window.addEventListener('deviceorientation', function(e) {
+      var heading = e.webkitCompassHeading + window.orientation;
+      if (Math.abs(heading - lastHeading)<180) {
+          spinner.style.webkitTransition = 'all 0.2s ease-in-out';
+      } else {
+          spinner.style.webkitTransition = 'none';
+      }
+      spinner.style.webkitTransform = 'rotateZ(-' + heading + 'deg)';
+      lastHeading = heading;
+  }, false);
+
+  document.body.addEventListener('touchstart', function(e) {
+      e.preventDefault();
+  }, false);
+
+  window.addEventListener('orientationchange', function(e) {
+      window.scrollTo(0,1);
+  }, false);
+
+  setTimeout(function () {window.scrollTo(0,1);}, 0);
+
+}, false);
+
+*/
+
+
+
+/*https://github.com/rheh/HTML5-canvas-projects/tree/master/compass*/
+// Global variable
+var img = null,
+	needle = null,
+	ctx = null,
+	degrees = 0;
+
+function clearCanvas() {
+	 // clear canvas
+	ctx.clearRect(0, 0, 200, 200);
+}
+
+function draw() {
+
+	clearCanvas();
+
+	// Draw the compass onto the canvas
+	ctx.drawImage(img, 0, 0);
+
+	// Save the current drawing state
+	ctx.save();
+
+	// Now move across and down half the 
+	ctx.translate(100, 100);
+
+	// Rotate around this point
+	ctx.rotate(degrees * (Math.PI / 180));
+
+	// Draw the image back and up
+	ctx.drawImage(needle, -100, -100);
+
+	// Restore the previous drawing state
+	ctx.restore();
+
+	// Increment the angle of the needle by 5 degrees
+	degrees += 5;
+}
+
+function imgLoaded() {
+	// Image loaded event complete.  Start the timer
+	setInterval(draw, 100);
+}
+
+function init() {
+	// Grab the compass element
+	var canvas = document.getElementById('compass');
+
+	// Canvas supported?
+	if (canvas.getContext('2d')) {
+		ctx = canvas.getContext('2d');
+
+		// Load the needle image
+		needle = new Image();
+		needle.src = 'needle.png';
+
+		// Load the compass image
+		img = new Image();
+		img.src = 'compass.png';
+		img.onload = imgLoaded;
+	} else {
+		alert("Canvas not supported!");
+	}
+}
+
+
