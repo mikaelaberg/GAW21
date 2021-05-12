834e1f572ea4d5e6163c0964c1f24be9639e128d
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-05-12 13:39:11 -0400

    stacked bar chart windrose

--- plotly/index.js
+++ plotly/index.js
@@ -1,26 +1,54 @@
 $(function(){
     var radarOptions = {
+        palette: "soft",
+        dataSource: dataSource[0].values,
+        title: "Wind Rose, Philadelphia PA",
+        commonSeriesSettings: {
+            type: "stackedbar"
+        },
         margin: {
-            top: 50,
             bottom: 50,
             left: 100
         },
-        dataSource: dataSource,
-        series: [{valueField: "day", name: "Day", color: "#ba4d51" }, 
-                 { valueField: "night", name: "Night", color: "#5f8b95" }],
-        commonSeriesSettings: {     
-            type: "scatter"
-        }
+        onLegendClick: function(e){
+            var series = e.target;
+            if (series.isVisible()) {
+                series.hide();
+            } else {
+                series.show();
+            }
+        },
+        argumentAxis: {
+            discreteAxisDivisionMode: "crossLabels",
+            firstPointOnStartAngle: true
+        }, 
+        valueAxis: {
+            valueMarginsEnabled: false
+        },
+        "export": {
+            enabled: true
+        },
+        series: [{ valueField: "val1", name: "1.3-4 m/s" },
+                { valueField: "val2", name: "4-8 m/s" },
+                { valueField: "val3", name: "8-13 m/s" },
+                { valueField: "val4", name: "13-19 m/s" },
+                { valueField: "val5", name: "19-25 m/s" },
+                { valueField: "val6", name: "25-32 m/s" },
+                { valueField: "val7", name: "32-39 m/s" },
+                { valueField: "val8", name: "39-47 m/s" }
+        ]
     };
     
     var radar = $("#radarChart").dxPolarChart(radarOptions).dxPolarChart("instance");
     
-    $("#radarTypes").dxSelectBox({
-        width: 200,
-        dataSource: types,
-        value: types[0],
+    $("#radarPeriods").dxSelectBox({
+        width: 300,
+        dataSource: dataSource,
+        displayExpr: "period",
+        valueExpr: "values",
+        value: dataSource[0].values,
         onValueChanged: function(e){
-            radar.option("commonSeriesSettings.type", e.value);        
+            radar.option("dataSource", e.value);
         }
     });
 });
