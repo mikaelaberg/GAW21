44d2833a5fb3661813fd115c05e33d30f06d13e1
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-05-25 18:02:08 -0400

    gtting tabelau public in the dash

--- plotly/stackedbarplotWaterTemp/data.js
+++ plotly/stackedbarplotWaterTemp/data.js
@@ -1,6 +1,16 @@
-var dataSource = [
+var dataSource1 = [
   {
     column: "Water Temp",
+    twoMeters: 59.8,
+    fourMeters: 56.2,
+    sevenMeters: 55.4,
+    nineMeters: 54.7,
+    elevenMeters: 53.9,
+  },
+];
+var dataSource2 = [
+  {
+    column: "Water DO",
     twoMeters: 59.8,
     fourMeters: 56.2,
     sevenMeters: 55.4,
--- plotly/stackedbarplotWaterTemp/index.js
+++ plotly/stackedbarplotWaterTemp/index.js
@@ -1,6 +1,6 @@
 $(function () {
-  $("#chart").dxChart({
-    dataSource: dataSource,
+  $("#chart1").dxChart({
+    dataSource: dataSource1,
     commonSeriesSettings: {
       argumentField: "column",
       type: "fullStackedBar",
@@ -22,6 +22,46 @@ $(function () {
       text: "Water Column",
       subtitle: {
         text: "Water Temperature ",
+      },
+    },
+    export: {
+      enabled: true,
+    },
+    tooltip: {
+      enabled: true,
+      customizeTooltip: function (arg) {
+        return {
+          text: arg.percentText + " - " + arg.valueText,
+        };
+      },
+    },
+  });
+});
+
+$(function () {
+  $("#chart2").dxChart({
+    dataSource: dataSource2,
+    commonSeriesSettings: {
+      argumentField: "column",
+      type: "fullStackedBar",
+    },
+    series: [
+      //color gradient hex https://www.schemecolor.com/orange-to-blue-gradient.php
+      { valueField: "elevenMeters", name: "11 Meters", color: "#0200C6" },
+      { valueField: "nineMeters", name: "9 Meters", color: "#412395" },
+      { valueField: "sevenMeters", name: "7 Meters", color: "#814663" },
+      { valueField: "fourMeters", name: "4 Meters", color: "#C06832" },
+      { valueField: "twoMeters", name: "2 Meters", color: "#FF8B00" },
+    ],
+    legend: {
+      verticalAlignment: "center",
+      horizontalAlignment: "left",
+      itemTextPosition: "right",
+    },
+    title: {
+      text: "Water Column",
+      subtitle: {
+        text: "Water DO ",
       },
     },
     export: {
