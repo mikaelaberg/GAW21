47785bcb1a10e4ea74fb9d4765d3d82fd8ff2de6
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-05-31 13:56:53 -0400

    dupdating with newest data and tabeau public graphs

--- AWRI_Hist_Dash/main.js
+++ AWRI_Hist_Dash/main.js
@@ -1,7 +1,7 @@
-function changeGraph(graphSource){
-    var imgagecontainer = document.getElementById("GraphBase");
-    imgagecontainer.src = graphSource;
-    document.getElementById("GraphBase").innerHtml = imgagecontainer;
+function changeGraph(graphSource) {
+  var imgagecontainer = document.getElementById("GraphBase");
+  imgagecontainer.src = graphSource;
+  document.getElementById("GraphBase").innerHtml = imgagecontainer;
 }
 
 // function appendData(data) {
@@ -13,90 +13,104 @@ function changeGraph(graphSource){
 //     }
 // }
 
-
 const json = (() => {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': "testdata.json", //Json file location
-        'dataType': "json",
-        'success': function(data) {
-        json = data;
-    }
-    });
-    return json;
+  var json = null;
+  $.ajax({
+    async: false,
+    global: false,
+    url: "testdata.json", //Json file location
+    dataType: "json",
+    success: function (data) {
+      json = data;
+    },
+  });
+  return json;
 })();
 
+document.getElementById("windspeed").textContent = `${json.WindSpeed} knots`;
+document.getElementById(
+  "winddirection"
+).textContent = `${json.WindDirection} degrees`;
 
-document.getElementById("windspeed").textContent = `${(json.WindSpeed)} knots`;
-document.getElementById("winddirection").textContent = `${(json.WindDirection)} degrees`;
+document.getElementById("airtemp").textContent = `${json.AirTemp} °F`;
+document.getElementById(
+  "relativehumidity"
+).textContent = `${json.RelativeHumidity}%`;
+document.getElementById(
+  "relativebarometricpressure"
+).textContent = `${json.RelativeBarometricPressure} Hg`;
+document.getElementById(
+  "raincurrently"
+).textContent = `${json.RainCurrently} inches`;
 
+document.getElementById(
+  "watertemp2m"
+).textContent = `${json.WaterTemp2m} °F 2 meters`;
+document.getElementById(
+  "watertemp4m"
+).textContent = `${json.WaterTemp4m} °F 4 meters`;
+document.getElementById(
+  "watertemp7m"
+).textContent = `${json.WaterTemp7m} °F 7 meters`;
+document.getElementById(
+  "watertemp9m"
+).textContent = `${json.WaterTemp9m} °F 9 meters`;
+document.getElementById(
+  "watertemp11m"
+).textContent = `${json.WaterTemp11m} °F 11 meters`;
 
-document.getElementById("airtemp").textContent = `${(json.AirTemp)} °F`;
-document.getElementById("relativehumidity").textContent = `${(json.RelativeHumidity)}%`;
-document.getElementById("relativebarometricpressure").textContent = `${(json.RelativeBarometricPressure)} Hg`;
-document.getElementById("raincurrently").textContent = `${(json.RainCurrently)} inches`;
-
-
-document.getElementById("watertemp2m").textContent = `${(json.WaterTemp2m)} °F 2 meters`;
-document.getElementById("watertemp4m").textContent = `${(json.WaterTemp4m)} °F 4 meters`;
-document.getElementById("watertemp7m").textContent = `${(json.WaterTemp7m)} °F 7 meters`;
-document.getElementById("watertemp9m").textContent = `${(json.WaterTemp9m)} °F 9 meters`;
-document.getElementById("watertemp11m").textContent = `${(json.WaterTemp11m)} °F 11 meters`;
-
-document.getElementById("dissolvedoxygen2m").textContent = `${(json.DissolvedOxygen2m)} mg/L 2 meters`;
-document.getElementById("dissolvedoxygen5m").textContent = `${(json.DissolvedOxygen5m)} mg/L 5 meters`;
-document.getElementById("dissolvedoxygen8m").textContent = `${(json.DissolvedOxygen8m)} mg/L 8 meters`;
-document.getElementById("dissolvedoxygen11m").textContent = `${(json.DissolvedOxygen11m)} mg/L 11 meters`;
+document.getElementById(
+  "dissolvedoxygen2m"
+).textContent = `${json.DissolvedOxygen2m} mg/L 2 meters`;
+document.getElementById(
+  "dissolvedoxygen5m"
+).textContent = `${json.DissolvedOxygen5m} mg/L 5 meters`;
+document.getElementById(
+  "dissolvedoxygen8m"
+).textContent = `${json.DissolvedOxygen8m} mg/L 8 meters`;
+document.getElementById(
+  "dissolvedoxygen11m"
+).textContent = `${json.DissolvedOxygen11m} mg/L 11 meters`;
 
 document.getElementById("Time").innerHTML = json.DateTime;
 
-
-
-
-
-
 // document.querySelector('.testData').textContent = json.AirTemp;
-$(document).ready(function(){
-    $('.gauge-wrap').simpleGauge({
-        width:'200',
-        hueLow:'1',
-        hueHigh:'128',
-        saturation:'100%',
-        lightness:'50%',
-        gaugeBG:'#1b1b1f',
-        parentBG:'#323138'
-        });
+$(document).ready(function () {
+  $(".gauge-wrap").simpleGauge({
+    width: "200",
+    hueLow: "1",
+    hueHigh: "128",
+    saturation: "100%",
+    lightness: "50%",
+    gaugeBG: "#1b1b1f",
+    parentBG: "#323138",
+  });
 });
-
 
 const gaugeElement1 = document.querySelector(".gauge1");
 
 function setGauge1Value(gauge, value) {
-
   gauge.querySelector(".gauge__fill1").style.transform = `rotate(${
-    value / -2 /*trial and error figured out that negative numbers flipped the bar to be what I needed for this */
+    value /
+    -2 /*trial and error figured out that negative numbers flipped the bar to be what I needed for this */
   }turn)`;
-  gauge.querySelector(".gauge__cover1").textContent = `${(json.Chlorophyll2m)
-  }µg/L`;
+  gauge.querySelector(
+    ".gauge__cover1"
+  ).textContent = `${json.Chlorophyll2m}µg/L`;
 }
 setGauge1Value(gaugeElement1, json.Chlorophyll2m);
 
-
 const gaugeElement2 = document.querySelector(".gauge2");
 function setGauge2Value(gauge, value) {
-    gauge.querySelector(".gauge__fill2").style.transform = `rotate(${
-      value / -23 /*trial and error number */
-    }turn)`;
-    gauge.querySelector(".gauge__cover2").textContent = `${(json.Phycocyanin2m)
-    }Cells/mL`;
-  }
+  gauge.querySelector(".gauge__fill2").style.transform = `rotate(${
+    value / -23 /*trial and error number */
+  }turn)`;
+  gauge.querySelector(
+    ".gauge__cover2"
+  ).textContent = `${json.Phycocyanin2m}Cells/mL`;
+}
 
-setGauge2Value(gaugeElement2,json.Phycocyanin2m);
-
-
-
+setGauge2Value(gaugeElement2, json.Phycocyanin2m);
 
 /*https://github.com/jamesgpearce/compios5/blob/master/index.html
 window.addEventListener('load', function() {
@@ -151,69 +165,65 @@ window.addEventListener('load', function() {
 
 */
 
-
-
 /*https://github.com/rheh/HTML5-canvas-projects/tree/master/compass*/
 // Global variable
 var img = null,
-	needle = null,
-	ctx = null,
-	degrees = 0;
+  needle = null,
+  ctx = null,
+  degrees = 0;
 
 function clearCanvas() {
-	 // clear canvas
-	ctx.clearRect(0, 0, 200, 200);
+  // clear canvas
+  ctx.clearRect(0, 0, 200, 200);
 }
 
 function draw() {
+  clearCanvas();
 
-	clearCanvas();
+  // Draw the compass onto the canvas
+  ctx.drawImage(img, 0, 0);
 
-	// Draw the compass onto the canvas
-	ctx.drawImage(img, 0, 0);
+  // Save the current drawing state
+  ctx.save();
 
-	// Save the current drawing state
-	ctx.save();
+  // Now move across and down half the
+  ctx.translate(100, 100);
 
-	// Now move across and down half the
-	ctx.translate(100, 100);
+  // Rotate around this point
+  ctx.rotate(degrees * (Math.PI / 180));
 
-	// Rotate around this point
-	ctx.rotate(degrees * (Math.PI / 180));
+  // Draw the image back and up
+  ctx.drawImage(needle, -100, -100);
 
-	// Draw the image back and up
-	ctx.drawImage(needle, -100, -100);
+  // Restore the previous drawing state
+  ctx.restore();
 
-	// Restore the previous drawing state
-	ctx.restore();
-
-	// Increment the angle of the needle by 5 degrees
-	degrees += 5;
+  // Increment the angle of the needle by 5 degrees
+  degrees += 5;
 }
 
 function imgLoaded() {
-	// Image loaded event complete.  Start the timer
-	setInterval(draw, 100);
+  // Image loaded event complete.  Start the timer
+  setInterval(draw, 100);
 }
 
 function init() {
-	// Grab the compass element
-	var canvas = document.getElementById('compass');
+  // Grab the compass element
+  var canvas = document.getElementById("compass");
 
-	// Canvas supported?
-	if (canvas.getContext('2d')) {
-		ctx = canvas.getContext('2d');
+  // Canvas supported?
+  if (canvas.getContext("2d")) {
+    ctx = canvas.getContext("2d");
 
-		// Load the needle image
-		needle = new Image();
-		needle.src = 'needle.png';
+    // Load the needle image
+    needle = new Image();
+    needle.src = "needle.png";
 
-		// Load the compass image
-		img = new Image();
-		img.src = 'compass.png';
-		img.onload = imgLoaded;
-	} else {
-		alert("Canvas not supported!");
-	}
+    // Load the compass image
+    img = new Image();
+    img.src = "compass.png";
+    img.onload = imgLoaded;
+  } else {
+    alert("Canvas not supported!");
+  }
 }
-
