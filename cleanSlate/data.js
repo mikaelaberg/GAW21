828c8c971346b35b20e091402ae4e178a8c08b6f
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-06-23 18:41:09 -0400

    api testing notes

--- cleanSlate/data.js
+++ cleanSlate/data.js
@@ -6,7 +6,7 @@
 
 async function test() {
   fetch(
-    "https://www.gvsu.edu/wri/buoy/data-generate.htm?concentration=1&date=6/19/21&format=json&graph=scatter&time=0:00,1:00,2:00,3:00,4:00,5:00,6:00,7:00,8:00,9:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00&x=date&y=atmp1,raincumulative,raincurrent,baro1,rh1,clcon001,odo001,phyco001,tp001,tp002,odo002,tp003,tp004,odo003,tp005,odo004,tp006",
+    "https://www.gvsu.edu/wri/buoy/data-generate.htm?concentration=1&date=6/23/21&format=json&graph=scatter&time=0:00,1:00,2:00,3:00,4:00,5:00,6:00,7:00,8:00,9:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00&x=date&y=atmp1,clcon001",
     {
       headers: {
         "Access-Control-Allow-Origin": "*",
@@ -26,11 +26,5 @@ async function test() {
 
 test();
 
-fetch(
-  "https://www.gvsu.edu/wri/buoy/data-generate.htm?concentration=1&date=6/19/21&format=json&graph=scatter&time=0:00,1:00,2:00,3:00,4:00,5:00,6:00,7:00,8:00,9:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00&x=date&y=atmp1,raincumulative,raincurrent,baro1,rh1,clcon001,odo001,phyco001,tp001,tp002,odo002,tp003,tp004,odo003,tp005,odo004,tp006",
-  { mode: "no-cors" }
-)
-  .then((res) => {
-    console.log("API :", res);
-  })
-  .catch();
+// this has to connect to the internet and this is most likely my issue I have to set up my VS code to the internet
+// write a piece of code to ping to just an internt (google) setting and if i can get it back with packets
