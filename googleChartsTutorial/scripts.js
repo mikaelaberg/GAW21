20daf7b9273059b55cedcbbd238173ead42804a6
mikaelaberg <61589685+mikaelaberg@users.noreply.github.com>
2021-08-10 15:38:01 -0400

    midDayWork

--- googleChartsTutorial/scripts.js
+++ googleChartsTutorial/scripts.js
@@ -1,1 +1,19 @@
-console.log(print("Hello World"));
+// Load Charts and the corechart package.
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart1);
+
+function drawChart1() {
+    // Define the chart to be drawn.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Element');
+    data.addColumn('number', 'Percentage');
+    data.addRows([
+      ['Nitrogen', 0.78],
+      ['Oxygen', 0.21],
+      ['Other', 0.01]
+    ]);
+
+    // Instantiate and draw the chart.
+    var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+    chart.draw(data, null);
+  }
